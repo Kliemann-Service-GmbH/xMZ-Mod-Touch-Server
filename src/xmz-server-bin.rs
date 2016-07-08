f284e34be2922c736b05b7dac6a50f27d1b01a52
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-08 12:54:43 +0200

    Vorbereitung für Test auf XMZ Hardware

--- src/xmz-server-bin.rs
+++ src/xmz-server-bin.rs
@@ -20,18 +20,18 @@ fn main() {
     server.modules.push(Module::new(ModuleType::RAGAS_CO_NO2));
     server.modules.push(Module::new(ModuleType::RAGAS_CO_NO2));
     server.modules.push(Module::new(ModuleType::RAGAS_CO_NO2));
-    server.modules[0].modbus_slave_id = 23;
-    server.modules[1].modbus_slave_id = 24;
-    server.modules[2].modbus_slave_id = 25;
-    server.modules[3].modbus_slave_id = 26;
-    server.modules[4].modbus_slave_id = 27;
-    server.modules[5].modbus_slave_id = 28;
+    server.modules[0].modbus_slave_id = 1;
+    server.modules[1].modbus_slave_id = 2;
+    server.modules[2].modbus_slave_id = 3;
+    server.modules[3].modbus_slave_id = 4;
+    server.modules[4].modbus_slave_id = 5;
+    server.modules[5].modbus_slave_id = 6;
 
     let server = Arc::new(RwLock::new(server));
     // Verschiedene Server Instanzen erzeugen, diese werden später in den Threads erneut geklont.
     let server1 = server.clone();
     let server2 = server.clone();
-    
+    let server3 = server.clone();
 
     let guard = thread::spawn(move || {
         loop {
@@ -61,8 +61,17 @@ fn main() {
             });
             worker_task.join();
 
-
-
+            // // 3. Task
+            // let server3 = server3.clone();
+            // let nanomsg_server = thread::spawn(move || {
+            //     let mut server = server3.write().expect("Fehler beim write lock des Servers");
+            //     if server.modules.len() > 10 { return };
+            //
+            //     let module = Module::new(ModuleType::RAGAS_CO_NO2);
+            //     server.modules.push(module);
+            //     thread::sleep(Duration::new(2, 0));
+            // });
+            // nanomsg_server.join();
 
         } // Ende loop
     });
