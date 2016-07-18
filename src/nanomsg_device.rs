d0aa51537ff7c8dc6a884042c44a2b2650c48bc3
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-18 11:43:45 +0200

    DeviceError angefangen und alles unwrap() aus dem NanomsgDevice entfernt

--- src/nanomsg_device.rs
+++ src/nanomsg_device.rs
@@ -1,17 +1,52 @@
 use nanomsg::{Protocol, Socket};
 use std::thread;
+use nanomsg;
+use std::error::Error;
+use std::fmt;
 
-pub struct NanoMsgDevice {
 
+#[derive(Debug)]
+pub enum DeviceError {
+    Nanomsg(nanomsg::Error),
 }
+
+impl fmt::Display for DeviceError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            DeviceError::Nanomsg(ref err) => err.fmt(f),
+        }
+    }
+}
+
+impl Error for DeviceError {
+    fn description(&self) -> &str {
+        match *self {
+            DeviceError::Nanomsg(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            DeviceError::Nanomsg(ref err) => Some(err),
+        }
+    }
+}
+
+impl From<nanomsg::Error> for DeviceError {
+    fn from(err: nanomsg::Error) -> DeviceError {
+        DeviceError::Nanomsg(err)
+    }
+}
+
+pub struct NanoMsgDevice {}
 
 impl NanoMsgDevice {
     /// Erzeugt und startet das NonoMsg Device
-    pub fn create() {
-        let mut front_socket = Socket::new_for_device(Protocol::Rep).unwrap();
-        let mut front_endpoint = front_socket.bind("ipc:///tmp/xmz-client.ipc").unwrap();
-        let mut back_socket = Socket::new_for_device(Protocol::Req).unwrap();
-        let mut back_endpoint = back_socket.bind("ipc:///tmp/xmz-server.ipc").unwrap();
+    pub fn create() -> Result<(), DeviceError> {
+        let mut front_socket   = try!(Socket::new_for_device(Protocol::Rep));
+        let mut front_endpoint = try!(front_socket.bind("ipc:///tmp/xmz-client.ipc"));
+        let mut back_socket    = try!(Socket::new_for_device(Protocol::Req));
+        let mut back_endpoint  = try!(back_socket.bind("ipc:///tmp/xmz-server.ipc"));
 
         let device_thread = thread::spawn(move || {
             println!("Device ist bereit.");
@@ -21,5 +56,6 @@ impl NanoMsgDevice {
             front_endpoint.shutdown();
             back_endpoint.shutdown();
         });
+        Ok(())
     }
 }
