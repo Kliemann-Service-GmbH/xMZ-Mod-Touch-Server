27fdd4000cec28c5659fe733107562fe8f2edfdc
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-27 13:52:33 +0200

    Versionsnummer ganz unten auf der Homepage

--- update-documentation-via-travis.sh
+++ update-documentation-via-travis.sh
@@ -23,6 +23,7 @@ git fetch upstream
 git checkout upstream/gh-pages
 
 git checkout upstream/master README.md
+echo -e "\nLast Update: **`date +%F-%T-%N`** Version: **${rev}**" >> README.md
 pandoc --css http://gist.githubusercontent.com/tleonardi/b04fde6538b4a3fe16a6/raw/5c9aaff140f79a76c39a3abcd368004119933612/github-pandoc.css --self-contained --highlight-style=tango -s -f markdown -t html5 -o index.html README.md
 git reset HEAD README.md
 rm README.md
