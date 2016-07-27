e279d0225021435d1cac7f804ffeceba1f6452cb
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-27 11:19:41 +0200

    Neue Version des Scripts ohne +x und mit separatem sudo Befehl für die Installation

--- install-nanomsg.sh
+++ install-nanomsg.sh
@@ -2,10 +2,12 @@
 set -ex
 
 cd /tmp
-wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz -O /tmp/nanomsg-.0.0.tar.gz
-tar xfvz /tmp/nanomsg-.0.0.tar.gz
-mkdir nanomsg-1.0.0/build && \
-  cd nanomsg-1.0.0/build && \
-    cmake .. && cmake --build . && \
-      sudo cmake --build . --target install
+wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz -O /tmp/nanomsg-1.0.0.tar.gz
+tar xfvz /tmp/nanomsg-1.0.0.tar.gz -C /tmp/nanomsg-1.0.0
+mkdir /tmp/nanomsg-1.0.0/build
+cd /tmp/nanomsg-1.0.0/build && \
+    cmake .. && cmake --build .
+    
+sudo -C "cd /tmp/nanomsg-1.0.0/build && cmake --build . --target install"
+
 sudo ldconfig
