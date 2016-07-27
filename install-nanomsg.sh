bac8dea790eeb62cf049445e32e7c9f68b5af75c
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-27 10:54:05 +0200

    Build nanomsg via Skript

---
+++ install-nanomsg.sh
@@ -0,0 +1,7 @@
+#!/bin/sg
+set -ex
+
+wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz -O /tmp/nanomsg-.0.0.tar.gz
+tar xfvz /tmp/nanomsg-.0.0.tar.gz
+cd nanomsg-1.0.0 && cmake && sudo cmake --build . --target install
+sudo ldconfig
