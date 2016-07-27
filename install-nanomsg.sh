01e6743bac5c0bd747d6390137cf900dc6989dda
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-27 11:22:08 +0200

    Script update

--- install-nanomsg.sh
+++ install-nanomsg.sh
@@ -2,12 +2,16 @@
 set -ex
 
 cd /tmp
+
 wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz -O /tmp/nanomsg-1.0.0.tar.gz
+
+mkdir -p /tmp/nanomsg-1.0.0/build
+
 tar xfvz /tmp/nanomsg-1.0.0.tar.gz -C /tmp/nanomsg-1.0.0
-mkdir /tmp/nanomsg-1.0.0/build
+
 cd /tmp/nanomsg-1.0.0/build && \
     cmake .. && cmake --build .
-    
+
 sudo -C "cd /tmp/nanomsg-1.0.0/build && cmake --build . --target install"
 
 sudo ldconfig
