5c5fe45bae24e9d53c8eb6ba89c8de18dee5bb99
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-27 11:13:04 +0200

    Typos und Logik Fehler im Skript behoben

--- install-nanomsg.sh
+++ install-nanomsg.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -ex
 
+cd /tmp
 wget https://github.com/nanomsg/nanomsg/archive/1.0.0.tar.gz -O /tmp/nanomsg-.0.0.tar.gz
 tar xfvz /tmp/nanomsg-.0.0.tar.gz
-cd nanomsg-1.0.0 && cmake && sudo cmake --build . --target install
+mkdir nanomsg-1.0.0/build && \
+  cd nanomsg-1.0.0/build && \
+    cmake .. && cmake --build . && \
+      sudo cmake --build . --target install
 sudo ldconfig
