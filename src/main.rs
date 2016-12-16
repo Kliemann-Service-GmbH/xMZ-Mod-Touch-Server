98eebfea4492471731bb9cb7c713f7cc0d83b157
Stefan Müller <s.mueller@it.kls-glt.de>
2016-12-16 10:26:51 +0100

    error-chain Crate eingebunden

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+extern crate xmz_server;
 
+use xmz_server::errors::*;
 
 fn main() {
-    println!("xMZ-Mod-Server");
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
 
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // If th backtrace is not generated. Try to run with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+    println!("xMZ-Mod-Touch-Server is running...");
+}
+
+fn run() -> Result<()> {
+    use std::fs::File;
+
+    // This operation will fail
+    File::open("xMZ-Mod-Touch-configuration.toml")
+        .chain_err(|| "unable to open the configuration file.")?;
+
+    Ok(())
 }
