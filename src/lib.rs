7f42a2e520f76b2f12cba25b61ee4dfcb2a2e391
Stefan Müller <s.mueller@it.kls-glt.de>
2016-12-29 16:06:32 +0100

    Main bereinigt und Funktionen nach lib.rb verschoben

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,36 @@ mod configuration;
 pub use self::configuration::*;
 pub use self::server::*;
 pub use self::error::*;
+
+
+pub fn mount_boot() -> Result<()> {
+    match system_commands::call("mount /dev/mmcblk0p1 /boot") {
+        Ok(_) => Ok(()),
+        Err(err) => Err(XMZError::NotAllowed),
+    }
+}
+
+pub fn umount_boot() -> Result<()> {
+    system_commands::call("umount /boot")?;
+
+    Ok(())
+}
+
+pub fn run() -> Result<()> {
+    let server = Server::new();
+
+    #[cfg(feature = "development")]
+    {
+        println!("Development System");
+    }
+
+    #[cfg(not(feature = "development"))]
+    {
+        println!("Produktiv System");
+        try!(mount_boot().map(|_| {
+            println!("Lese nun Configuration");
+        }));
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,6 @@ use xmz_server::*;
 use xmz_server::system_commands;
 
 
-pub fn mount_boot() -> Result<()> {
-    match system_commands::call("mount /dev/mmcblk0p1 /boot") {
-        Ok(_) => Ok(()),
-        Err(err) => Err(XMZError::NotAllowed),
-    }
-}
-
-pub fn umount_boot() -> Result<()> {
-    system_commands::call("umount /boot")?;
-
-    Ok(())
-}
-
-fn run() -> Result<()> {
-    let server = Server::new();
-
-    #[cfg(feature = "development")]
-    {
-        println!("Development System");
-    }
-
-    #[cfg(not(feature = "development"))]
-    {
-        println!("Produktiv System");
-        try!(mount_boot().map(|_| {
-            println!("Lese nun Configuration");
-        }));
-    }
-
-    Ok(())
-}
-
 fn main() {
     println!("xMZ-Mod-Touch-Server Version: {}\n",
              env!("CARGO_PKG_VERSION"));
