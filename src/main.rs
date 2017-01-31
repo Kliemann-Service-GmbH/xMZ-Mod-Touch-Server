6ff4ac3a61b9915347ccbf502c82172b6e781928
Stefan Müller <s.mueller@it.kls-glt.de>
2017-01-31 13:35:31 +0100

    Requests werden mit 'info!()' geloggt

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ fn server_web_interface(server: Arc<Mutex<Server>>) -> Result<()> {
     router.get("/",                 move |r: &mut Request| index(r,    &server.lock().unwrap()), "index");
 
     fn index(request: &mut Request, server: &Server) -> IronResult<Response> {
-        debug!("{:#?}", request);
+        info!("{:#?}", request);
         let payload = serde_json::to_string(&server).unwrap();
         Ok(Response::with((status::Ok, payload)))
     }
