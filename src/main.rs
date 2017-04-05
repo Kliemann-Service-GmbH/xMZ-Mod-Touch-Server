472b78e9fb4bcd7ebc5f5aeed0bc710adb3cf58d
Stefan Müller <s.mueller@it.kls-glt.de>
2017-04-05 13:18:00 +0200

    Git hook script eingepflegt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-
+#![doc(html_logo_url = "https://raw.githubusercontent.com/Kliemann-Service-GmbH/xMZ-Mod-Touch-Server/master/share/xmz-logo.png",
+       html_favicon_url = "https://raw.githubusercontent.com/Kliemann-Service-GmbH/xMZ-Mod-Touch-Server/master/share/favicon.ico",
+       html_root_url = "https://gaswarnanlagen.com/")]
+//! # Serverteil der 'xMZ-Mod-Touch'-Platform&nbsp;[![Build Status](https://travis-ci.org/Kliemann-Service-GmbH/xMZ-Mod-Touch-Server.svg?branch=master)](https://travis-ci.org/Kliemann-Service-GmbH/xMZ-Mod-Touch-Server)
+//! [Projektseite |][homepage]&nbsp;[Dokumentation |][doku]&nbsp;[Repo auf Github.com |][repo]
+//!
+//! Dies ist der Hauptprozess der sehr früh, noch vor der GUI, gestartet wird.
+//! Er steuert die Hardware (LEDs, Relais, Akku usw.) und händelt die angeschlossenen
+//! Module und deren Sensoren.
+//!
+//! [1]: https://github.com/Kliemann-Service-GmbH/xMZ-Mod-Touch-Software
+//! [homepage]: http://kliemann-service-gmbh.github.io/xMZ-Mod-Touch-Server
+//! [repo]: https://github.com/Kliemann-Service-GmbH/xMZ-Mod-Touch-Server
+//! [doku]: http://kliemann-service-gmbh.github.io/xMZ-Mod-Touch-Server/target/doc/xmz_server/index.html
 
 
 fn main() {
     println!("xMZ-Mod-Touch-Server startet ...");
-    
+
 }
