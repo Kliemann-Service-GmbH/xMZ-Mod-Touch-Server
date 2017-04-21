e555901ee6ac02d182f391b107c399cd06029eda
Stefan Müller <s.mueller@it.kls-glt.de>
2017-04-21 13:24:42 +0200

    curl http://localhost:3000/api/v1/zone/[:zone_id] implementiert

--- src/json_api.rs
+++ src/json_api.rs
@@ -33,24 +33,30 @@ pub fn init(xmz_server: Arc<Mutex<XMZServer>>) -> Result<(), XMZServerError> {
 
     /// Catch All Route
     ///
-    /// curl http://localhost:3000
+    /// `curl http://localhost:3000`
     let xmz_server_clone = xmz_server.clone();
     router.get("/*",
                move |req: &mut Request| index(req, xmz_server_clone.clone()),
                "index");
-    /// curl http://localhost:3000/api/v1
+    /// `curl http://localhost:3000/api/v1`
     let xmz_server_clone = xmz_server.clone();
     router.get("/api/v1",
                move |req: &mut Request| index(req, xmz_server_clone.clone()),
                "index_with_api");
 
-    /// curl http://localhost:3000/api/v1/zones
+    /// `curl http://localhost:3000/api/v1/zones`
     let xmz_server_clone = xmz_server.clone();
     router.get("/api/v1/zones",
                move |req: &mut Request| zones_index(req, xmz_server_clone.clone()),
                "zones_index");
 
-    /// curl http://localhost:3000/api/v1/exceptions
+    /// `curl http://localhost:3000/api/v1/zone/0`
+    let xmz_server_clone = xmz_server.clone();
+    router.get("/api/v1/zone/:zone_id",
+               move |req: &mut Request| zone_get(req, xmz_server_clone.clone()),
+               "zone_get");
+
+    /// `curl http://localhost:3000/api/v1/exceptions`
     let xmz_server_clone = xmz_server.clone();
     router.get("/api/v1/exceptions",
                move |req: &mut Request| exceptions_index(req, xmz_server_clone.clone()),
@@ -68,6 +74,19 @@ pub fn init(xmz_server: Arc<Mutex<XMZServer>>) -> Result<(), XMZServerError> {
     fn zones_index(_req: &mut Request, xmz_server: Arc<Mutex<XMZServer>>) -> IronResult<Response> {
         if let Ok(xmz_server) = xmz_server.lock() {
             let payload = serde_json::to_string_pretty(&*xmz_server.get_zones()).unwrap();
+            Ok(Response::with((status::Ok, payload)))
+        } else {
+            Err(IronError::new(StringError("Mutex XMZServer lock failed"), status::BadRequest))
+        }
+    }
+
+    fn zone_get(req: &mut Request, xmz_server: Arc<Mutex<XMZServer>>) -> IronResult<Response> {
+        if let Ok(xmz_server) = xmz_server.lock() {
+            // Extract the parameter(s)
+            let zone_id = req.extensions.get::<Router>()
+                .unwrap().find("zone_id").unwrap_or("0").parse::<usize>().unwrap_or(0);
+
+            let payload = serde_json::to_string_pretty(&xmz_server.get_zone(zone_id)).unwrap();
             Ok(Response::with((status::Ok, payload)))
         } else {
             Err(IronError::new(StringError("Mutex XMZServer lock failed"), status::BadRequest))
