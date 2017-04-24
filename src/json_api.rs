2ecd4b2b6ae7ff90b10538b26f22774731ed3730
Stefan Müller <s.mueller@it.kls-glt.de>
2017-04-24 09:39:16 +0200

    Webserver Adresse geöndert, Sensor LED und RELAIS richtig geschalten

--- src/json_api.rs
+++ src/json_api.rs
@@ -37,7 +37,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
 
     /// Index Route
     ///
-    /// `curl http://localhost:3000`
+    /// `curl http://0.0.0.0:3000`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/",
                move |req: &mut Request| index(req, xmz_mod_touch_server_clone.clone()),
@@ -49,31 +49,31 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
     /// handler Funktionen (index, zones_index, kombisensor_get, ...) existieren
     /// auf die Index Funktion weiter.
     ///
-    /// `curl http://localhost:3000`
+    /// `curl http://0.0.0.0:3000`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/*",
                move |req: &mut Request| index(req, xmz_mod_touch_server_clone.clone()),
                "catch_all");
 
-    /// `curl http://localhost:3000/api/v1`
+    /// `curl http://0.0.0.0:3000/api/v1`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1",
                move |req: &mut Request| index(req, xmz_mod_touch_server_clone.clone()),
                "index_with_api");
 
-    /// `curl http://localhost:3000/api/v1/zones`
+    /// `curl http://0.0.0.0:3000/api/v1/zones`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1/zones",
                move |req: &mut Request| zones_index(req, xmz_mod_touch_server_clone.clone()),
                "zones_index");
 
-    /// `curl http://localhost:3000/api/v1/zone/0`
+    /// `curl http://0.0.0.0:3000/api/v1/zone/0`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1/zone/:zone_id",
                move |req: &mut Request| zone_get(req, xmz_mod_touch_server_clone.clone()),
                "zone_get");
 
-    /// `curl http://localhost:3000/api/v1/zone/0/kombisensors`
+    /// `curl http://0.0.0.0:3000/api/v1/zone/0/kombisensors`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1/zone/:zone_id/kombisensors",
                move |req: &mut Request| {
@@ -81,31 +81,31 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                },
                "kombisensors_index");
 
-    /// `curl http://localhost:3000/api/v1/zone/0/kombisensor/0`
+    /// `curl http://0.0.0.0:3000/api/v1/zone/0/kombisensor/0`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1/zone/:zone_id/kombisensor/:kombisensor_id",
                move |req: &mut Request| kombisensor_get(req, xmz_mod_touch_server_clone.clone()),
                "kombisensor_get");
 
-    /// `curl http://localhost:3000/api/v1/zone/0/kombisensor/0/sensors`
+    /// `curl http://0.0.0.0:3000/api/v1/zone/0/kombisensor/0/sensors`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1/zone/:zone_id/kombisensor/:kombisensor_id/sensors",
                move |req: &mut Request| sensors_index(req, xmz_mod_touch_server_clone.clone()),
                "sensors_index");
 
-    /// `curl http://localhost:3000/api/v1/zone/0/kombisensor/0/sensor/0`
+    /// `curl http://0.0.0.0:3000/api/v1/zone/0/kombisensor/0/sensor/0`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1/zone/:zone_id/kombisensor/:kombisensor_id/sensor/:sensor_id",
                move |req: &mut Request| sensor_get(req, xmz_mod_touch_server_clone.clone()),
                "sensor_get");
 
-    /// `curl http://localhost:3000/api/v1/exceptions`
+    /// `curl http://0.0.0.0:3000/api/v1/exceptions`
     let xmz_mod_touch_server_clone = xmz_mod_touch_server.clone();
     router.get("/api/v1/exceptions",
                move |req: &mut Request| exceptions_index(req, xmz_mod_touch_server_clone.clone()),
                "exceptions_index");
 
-    /// Beispiel URL: http://localhost:3000/api/v1
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1
     fn index(_req: &mut Request,
              xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
              -> IronResult<Response> {
@@ -118,7 +118,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    /// Beispiel URL: http://localhost:3000/api/v1/zones
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1/zones
     fn zones_index(_req: &mut Request,
                    xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                    -> IronResult<Response> {
@@ -131,7 +131,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    /// Beispiel URL: http://localhost:3000/api/v1/zone/0
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1/zone/0
     fn zone_get(req: &mut Request,
                 xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                 -> IronResult<Response> {
@@ -154,7 +154,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    /// Beispiel URL: http://localhost:3000/api/v1/zone/0/kombisensors
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1/zone/0/kombisensors
     fn kombisensors_index(req: &mut Request,
                           xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                           -> IronResult<Response> {
@@ -180,7 +180,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    /// Beispiel URL: http://localhost:3000/api/v1/zone/0/kombisensor/0
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1/zone/0/kombisensor/0
     fn kombisensor_get(req: &mut Request,
                        xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                        -> IronResult<Response> {
@@ -213,7 +213,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    /// Beispiel URL: http://localhost:3000/api/v1/zone/0/kombisensor/0/sensors
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1/zone/0/kombisensor/0/sensors
     fn sensors_index(req: &mut Request,
                      xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                      -> IronResult<Response> {
@@ -247,7 +247,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    /// Beispiel URL: http://localhost:3000/api/v1/zone/0/kombisensor/0/sensor/0
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1/zone/0/kombisensor/0/sensor/0
     fn sensor_get(req: &mut Request,
                   xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                   -> IronResult<Response> {
@@ -289,7 +289,7 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    /// Beispiel URL: http://localhost:3000/api/v1/exceptions
+    /// Beispiel URL: http://0.0.0.0:3000/api/v1/exceptions
     fn exceptions_index(_req: &mut Request,
                         xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
                         -> IronResult<Response> {
@@ -303,8 +303,8 @@ pub fn init(xmz_mod_touch_server: Arc<Mutex<XMZModTouchServer>>)
         }
     }
 
-    println!("Webinterface: http://localhost:3000");
-    Iron::new(router).http("localhost:3000").unwrap();
+    println!("Webinterface: http://0.0.0.0:3000");
+    Iron::new(router).http("0.0.0.0:3000").unwrap();
 
     Ok(())
 }
