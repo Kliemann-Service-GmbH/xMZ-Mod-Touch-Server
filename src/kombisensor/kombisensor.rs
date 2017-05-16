311605cf704f58bea2d2e8a1c2d8c8e949186ef1
zzeroo <co@zzeroo.com>
2017-05-16 18:02:22 +0200

    Import kombisensor from v0.20.6

--- src/kombisensor/kombisensor.rs
+++ src/kombisensor/kombisensor.rs
@@ -10,6 +10,8 @@ use kombisensor::Sensor;
 #[derive(Debug)]
 #[derive(Serialize, Deserialize)]
 pub struct Kombisensor {
+    firmware_version: String,
+    modbus_address: u8,
     sensors: Vec<Sensor>,
 }
 
@@ -32,6 +34,9 @@ impl Kombisensor {
     /// ```
     pub fn new() -> Self {
         Kombisensor {
+            firmware_version: "0.0.0".to_string(),
+            modbus_address: 247,
+            // TODO: Remove this two default sensors, if the config generator is working
             sensors: vec![
                 Sensor::new(),
                 Sensor::new(),
@@ -85,6 +90,84 @@ impl Kombisensor {
         for (num, mut sensor) in &mut self.sensors.iter_mut().enumerate() {
             sensor.update(num_zone, num, exceptions, leds, relais);
         }
+    }
+
+    /// Get firmware version
+    ///
+    /// # Return values
+    ///
+    /// Liefert die aktuelle Firmware Version die aus dem Sensor ausgelesen wurde,
+    /// eine Version "0.0.0" deutet darauf hin das der Kombisensor noch nie über Modbus ausgelesen wurde.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use xmz_mod_touch_server::Kombisensor;
+    ///
+    /// let kombisensor = Kombisensor::new();
+    /// assert_eq!(kombisensor.get_firmware_version(), "0.0.0");
+    /// ```
+    pub fn get_firmware_version(&self) -> String {
+        self.firmware_version.clone()
+    }
+
+    /// Set firmware version
+    ///
+    /// # Parameters
+    ///
+    /// * `firmware_version`    - String mit der neuen Firmware Version
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use xmz_mod_touch_server::Kombisensor;
+    ///
+    /// let mut kombisensor = Kombisensor::new();
+    /// assert_eq!(kombisensor.get_firmware_version(), "0.0.0");
+    /// kombisensor.set_firmware_version("9.9.99".to_string());
+    /// assert_eq!(kombisensor.get_firmware_version(), "9.9.99");
+    /// ```
+    pub fn set_firmware_version(&mut self, firmware_version: String) {
+        self.firmware_version = firmware_version
+    }
+
+    /// Get modbus_address
+    ///
+    /// # Return values
+    ///
+    /// Liefert die aktuelle Firmware Version die aus dem Sensor ausgelesen wurde,
+    /// eine Version "0.0.0" deutet darauf hin das der Kombisensor noch nie über Modbus ausgelesen wurde.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use xmz_mod_touch_server::Kombisensor;
+    ///
+    /// let kombisensor = Kombisensor::new();
+    /// assert_eq!(kombisensor.get_modbus_address(), 247);
+    /// ```
+    pub fn get_modbus_address(&self) -> u8 {
+        self.modbus_address
+    }
+
+    /// Set modbus_address
+    ///
+    /// # Parameters
+    ///
+    /// * `modbus_address`    - u8 mit der neuen Modbus Adresse
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use xmz_mod_touch_server::Kombisensor;
+    ///
+    /// let mut kombisensor = Kombisensor::new();
+    /// assert_eq!(kombisensor.get_modbus_address(), 247);
+    /// kombisensor.set_modbus_address(1);
+    /// assert_eq!(kombisensor.get_modbus_address(), 1);
+    /// ```
+    pub fn set_modbus_address(&mut self, modbus_address: u8) {
+        self.modbus_address = modbus_address
     }
 
     ///
