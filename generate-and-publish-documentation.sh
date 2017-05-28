9573b8f902e73116b161ac380c919a0e841bb6a3
zzeroo <co@zzeroo.com>
2017-05-28 15:30:27 +0200

    One additional push

--- generate-and-publish-documentation.sh
+++ generate-and-publish-documentation.sh
@@ -78,6 +78,7 @@ rm target
 # Add all (new) files to git and commit them.
 git add .
 git commit -m "Update docs for $last_rev" -m "$last_msg"
+git push
 
 cd $dir
 git push --set-upstream origin "$DOC_BRANCH"
